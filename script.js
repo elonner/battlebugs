5eb87b08c24e31b9599452fdfc26ba490ebff77f
elonner <elilnnr5@gmail.com>
2023-04-22 19:52:42 -0700

    refactors code to use jQuery wherever possible, other organization/optimization

--- script.js
+++ script.js
@@ -2,14 +2,15 @@
 const MOVE_DELAY = 200;
 
 //=================================================== STATE VARIABLES ===========================================================
-let userBoard;
+let difficulty;
 let cpuBoard;
-let userBugs;
+let userBoard;
 let cpuBugs;
+let userBugs;
+let selectedBug;
+let playing;
 let turn; // 1, -1
 let winner; // null, 1: user, -1: cpu
-let playing;
-let selectedBug;
 
 //======================================================= CLASSES =================================================================
 class Cell {
@@ -19,9 +20,11 @@ class Cell {
         this.col = pos[1];
         this.color = 'white';
         this.isOccupied = false;
-        var el = document.createElement('div');
-        el.classList.add('cell');
-        this.el = el;
+
+        this.$ = $('<div class="cell"></div>');
+        // var el = document.createElement('div');
+        // el.classList.add('cell');
+        // this.el = el;
         this.bug; // bug that is covering cell 
     }
 }
@@ -30,9 +33,11 @@ class Bug {
     constructor(size) {
         this.size = size; // 5, 4, 3, or 2
         this.orient = 1; // 1: vertical, -1: horizontal
-        this.el = document.createElement('img');
-        this.el.setAttribute('src', `Icons/${size}er.png`);
-        this.el.classList.add('bug', `${size}`);
+
+        this.$ = $(`<img src="Icons/${size}er.png" class="bug ${size}">`);
+        // this.el = document.createElement('img');
+        // this.el.setAttribute('src', `Icons/${size}er.png`);
+        // this.el.classList.add('bug', `${size}`);
         this.row; // top row
         this.col; // left column 
         this.hits = 0; // total hits
@@ -43,15 +48,21 @@ class Bug {
 }
 
 //====================================================== CACHE =========================================================
-const userBoardEl = document.getElementById('user-board');
-const cpuBoardEl = document.getElementById('cpu-board');
+const $userBoard = $('#user-board');
+const $cpuBoard = $('#cpu-board');
+// const userBoardEl = document.getElementById('user-board');
+// const cpuBoardEl = document.getElementById('cpu-board');
+
 const $bugBox = $('#bug-box');
 
 //====================================================== EVENT LISTENERS ================================================
 $bugBox.on('click', '.bug', selectBug);
 $(document).on('keyup', delegateEvent);
-$('#user-board').on('click', '.cell', placeBug); 
-$('#cpu-board').on('click', '.cell', userShot);
+
+$userBoard.on('click', '.cell', placeBug);
+$cpuBoard.on('click', '.cell', userShot);
+// $('#user-board').on('click', '.cell', placeBug); 
+// $('#cpu-board').on('click', '.cell', userShot);
 
 //====================================================== MAIN =====================================================
 init();
@@ -182,24 +193,37 @@ function placeUserBugs() {
 
 //==================================================== MOVE HANDLERS ==============================================
 function selectBug() {
-    const $bug = $(this)[0];
-    if ($bug.classList.contains('selected')) {                          // DESELECT
-        selectedBug = null;
-        $bug.classList.remove('selected');
-    } else {
-        if (selectedBug) selectedBug.el.classList.remove('selected');   // DESELECT then SELECT
-        selectedBug = userBugs.find(bug => bug.el === $bug);
-        $bug.classList.add('selected');
+    const $bug = $(this);
+    if (selectedBug && $bug[0] === selectedBug.$[0]) {
+        selectedBug.$.removeClass('selected');                    // DESELECT click
+        selectedBug = null;     
+    } else {                                                      // SELECT click
+        if (selectedBug) selectedBug.$.removeClass('selected');   
+        $bug.addClass('selected');
+        selectedBug = userBugs.find(bug => bug.$[0] === $bug[0]);  
     }
 }
+// function selectBug() {
+//     const bugEl = $(this)[0];
+//     if (bugEl.classList.contains('selected')) {                          // DESELECT
+//         selectedBug = null;
+//         bugEl.classList.remove('selected');
+//     } else {
+//         if (selectedBug) selectedBug.el.classList.remove('selected');   // DESELECT then SELECT
+//         selectedBug = userBugs.find(bug => bug.el === bugEl);
+//         bugEl.classList.add('selected');
+//     }
+// }
 
 function rotateBug(bug) {
     if (bug.orient === 1) {
-        bug.el.classList.remove('vertical');
-        bug.el.classList.add('horizontal');
+        bug.$.removeClass('vertical').addClass('horizontal');
+        // bug.el.classList.remove('vertical');
+        // bug.el.classList.add('horizontal');
     } else {
-        bug.el.classList.remove('horizontal');
-        bug.el.classList.add('vertical');
+        bug.$.removeClass('horizontal').addClass('vertical');
+        // bug.el.classList.remove('horizontal');
+        // bug.el.classList.add('vertical');
     }
     bug.orient *= (-1);
 }
@@ -219,18 +243,26 @@ function placeBug() {
                     userBoard[i][c].bug = selectedBug;
                     selectedBug.cellsOn.push(userBoard[i][c]);
                 }
-                selectedBug.el.setAttribute('style', `grid-column: ${selectedBug.col + 1}; grid-row: ${selectedBug.row + 1} / span ${selectedBug.size};`);
+                selectedBug.$.css({ gridArea: `${r+1} / ${c+1} / ${r+1+l} / ${c+1}`});
+                // selectedBug.$.css({ gridColumn: `${selectedBug.col + 1}`, gridRrow: `${selectedBug.row + 1} / span ${selectedBug.size}` });
+                // selectedBug.el.setAttribute('style', `grid-column: ${selectedBug.col + 1}; grid-row: ${selectedBug.row + 1} / span ${selectedBug.size};`);
             } else {
                 for (let i = c; i < c + l; i++) {
                     userBoard[r][i].isOccupied = true;
                     userBoard[r][i].bug = selectedBug;
                     selectedBug.cellsOn.push(userBoard[r][i]);
                 }
-                selectedBug.el.setAttribute('style', `grid-column: ${selectedBug.col + 1} / span ${selectedBug.size}; grid-row: ${selectedBug.row + 1};`);
+                selectedBug.$.css({ gridArea: `${r+1} / ${c+1} / ${r+1} / ${c+1+l}`});
+                // selectedBug.$.css({ gridColumn: `${selectedBug.col + 1} / span ${selectedBug.size}`, gridRow: `${selectedBug.row + 1}` });
+                // selectedBug.el.setAttribute('style', `grid-column: ${selectedBug.col + 1} / span ${selectedBug.size}; grid-row: ${selectedBug.row + 1};`);
             }
-            userBoardEl.append(selectedBug.el);
+            $userBoard.append(selectedBug.$);
+            $userBoard.append(selectedBug.$);
+            selectedBug.$.removeClass('selected');
+            // userBoardEl.append(selectedBug.el);
+            // userBoardEl.append(selectedBug.el);
+            // selectedBug.el.classList.remove('selected');
             selectedBug.isPlaced = true;
-            selectedBug.el.classList.remove('selected');
         }
     }
 }
@@ -308,29 +340,26 @@ function delegateEvent(e) {
 
 //================================================== RENDERERS ============================================================
 function initialRender() {
-    if (!playing) {                                
+    if (!playing) {
         userBugs.forEach(bug => {                   // ADD bugs to box
-            bug.el.classList.add('vertical');
-            $bugBox.append(bug.el);
+            bug.$.addClass('vertical');
+            $bugBox.append(bug.$);
+            // bug.el.classList.add('vertical');
+            // $bugBox.append(bug.el);
         });
         cpuBoard.forEach(row => {                   // ADD cells to boards
             row.forEach(cell => {
-                addCelltoBoard(cpuBoardEl, cell);
+                addCelltoBoard($cpuBoard, cell);
+                // addCelltoBoard(cpuBoardEl, cell);
             });
         });
         userBoard.forEach(row => {
             row.forEach(cell => {
-                addCelltoBoard(userBoardEl, cell);
+                addCelltoBoard($userBoard, cell);
+                // addCelltoBoard(userBoardEl, cell);
             });
         });
-    }  
-}
-
-function addCelltoBoard(boardEl, cell) {
-    cell.el.setAttribute('id', `r${cell.row}c${cell.col}`)
-    cell.el.style.gridColumn = `${cell.col + 1} / ${cell.col + 2}`;
-    cell.el.style.gridRow = `${cell.row + 1} / ${cell.row + 2}`;
-    boardEl.append(cell.el);
+    }
 }
 
 function render() {
@@ -341,7 +370,8 @@ function render() {
 // TODO: move the r,c setting and appending to an initializer so its not 
 function renderBoards() {
     if (playing) {                                  // HOVER
-        userBoardEl.classList.remove('hover');
+        $userBoard.removeClass('hover');
+        // userBoardEl.classList.remove('hover');
         $bugBox.toggle();
     }
     if (winner) {                                   // WINNER
@@ -351,50 +381,64 @@ function renderBoards() {
     cpuBoard.forEach(row => {
         row.forEach(cell => {
             if (cell.value === 1) {
-                cell.el.style.backgroundColor = 'red';
+                cell.$.css({ backgroundColor: 'red' });
+                // cell.el.style.backgroundColor = 'red';
             }
             if (cell.value === -1) {
-                cell.el.style.backgroundColor = 'green';
+                cell.$.css({ backgroundColor: 'green' });
+                // cell.el.style.backgroundColor = 'green';
             }
         });
     });
     userBoard.forEach(row => {
         row.forEach(cell => {
             if (cell.value === 1) {
-                cell.el.style.backgroundColor = 'red';
-                cell.el.style.zIndex = '1';
+                cell.$.css({ backgroundColor: 'red', zIndex: '1' });
+                // cell.el.style.backgroundColor = 'red';
+                // cell.el.style.zIndex = '1';
             }
             if (cell.value === -1) {
-                cell.el.style.backgroundColor = 'green';
-                cell.el.style.zIndex = '1';
+                cell.$.css({ backgroundColor: 'green', zIndex: '1' });
+                // cell.el.style.backgroundColor = 'green';
+                // cell.el.style.zIndex = '1';
             }
         });
     });
 }
 
-// TODO: wtf is going on with these horizontals 
+// TODO: wtf is going on with these horizontals (classes are added/removed in rotateBug)
 function renderBugs() {
-    if (playing) {                           
+    if (playing) {
         userBugs.forEach(bug => {
-            if (bug.type === 'user') {
-                userBoardEl.append(this.el);
-            } else if (bug.type === 'cpu') {
-                cpuBoardEl.append(this.el);
-            }
             if (bug.orient === 1) { // vertical
-                bug.el.classList.add('vertical');
-                bug.el.setAttribute('style', `grid-column: ${bug.col + 1}; grid-row: ${bug.row + 1} / span ${bug.size};`);
-                userBoardEl.append(bug.el);
+                bug.$.css({ gridColumn: `${bug.col + 1}`, gridRow: `${bug.row + 1} / span ${bug.size}` });
+                // bug.el.classList.add('vertical');
+                // bug.el.setAttribute('style', `grid-column: ${bug.col + 1}; grid-row: ${bug.row + 1} / span ${bug.size};`);
+                // userBoardEl.append(bug.el);
             } else { // horizontal (-1)
-                bug.el.classList.add('horizontal');
-                bug.el.setAttribute('style', `grid-column: ${bug.col + 1} / span ${bug.size}; grid-row: ${bug.row + 1};`);
-                userBoardEl.append(bug.el);
+                bug.$.css({ gridColumn: `${bug.col + 1} / span ${bug.size}`, gridRow: `${bug.row + 1}` });
+                // bug.el.classList.add('horizontal');
+                // bug.el.setAttribute('style', `grid-column: ${bug.col + 1} / span ${bug.size}; grid-row: ${bug.row + 1};`);
+                // userBoardEl.append(bug.el);
             }
+            $userBoard.append(bug.$);
         });
     }
 }
 
 //================================================ HELPERS ================================================================
+function addCelltoBoard($board, cell) {
+    cell.$.attr('id', `r${cell.row}c${cell.col}`);
+    cell.$.css({ gridColumn: `${cell.col + 1} / ${cell.col + 2}`, gridRow: `${cell.row + 1} / ${cell.row + 2}` });
+    $board.append(cell.$);
+}
+// function addCelltoBoard(boardEl, cell) {
+//     cell.el.setAttribute('id', `r${cell.row}c${cell.col}`)
+//     cell.el.style.gridColumn = `${cell.col + 1} / ${cell.col + 2}`;
+//     cell.el.style.gridRow = `${cell.row + 1} / ${cell.row + 2}`;
+//     boardEl.append(cell.el);
+// }
+
 function getWinner() {
     if (cpuBugs.every(bug => bug.isSquashed)) {
         return 1;
